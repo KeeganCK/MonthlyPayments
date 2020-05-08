90159a603445e1d681b3b47f012d1f3aa761a805
= <KeeganKavanagh@live.com>
2020-05-07 22:42:08 -0700

    didn't have .git in backend

--- frontend/src/Components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = props => {
-	return <div>
-			<button className={`btn ${props.class}`} onClick={props.onClick} style={props.style}>{props.name}</button>
-		</div>
-}
-
-export default Button;
